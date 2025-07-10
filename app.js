5f23daa9e173957a2165790a75ab342e3c3d072d
WasiQ-dev <muhammadwasiq67585@gmail.com>
2025-07-10 16:40:35 +0500

    datatypes/enum/array/tuple

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("Hello, TypeScript!");
+"use strict";
+// console.log("Hello, TypeScript!");
+// // Array//
+// let arr:number[]=[1, 2, 3, 4, 5];
+// console.log(arr);
+// // Tuple//
+// let tuple:[String,number]=["hello",12];
+// console.log(tuple);
+//Enums//
+var UserRoles;
+(function (UserRoles) {
+    UserRoles["admin"] = "admin";
+    UserRoles["super_Admin"] = "super_Admin";
+    UserRoles["manager"] = "manager";
+})(UserRoles || (UserRoles = {}));
+console.log(UserRoles.admin);
+var Status;
+(function (Status) {
+    Status["aboundend"] = "500";
+    Status["notFound"] = "404";
+    Status["Valid"] = "200";
+})(Status || (Status = {}));
+console.log(Status.Valid);
--- app.ts
+++ app.ts
@@ -1,2 +1,25 @@
-console.log("Hello, TypeScript!");
+// console.log("Hello, TypeScript!");
+// // Array//
 
+// let arr:number[]=[1, 2, 3, 4, 5];
+// console.log(arr);
+
+// // Tuple//
+// let tuple:[String,number]=["hello",12];
+// console.log(tuple);
+
+//Enums//
+
+enum UserRoles {
+    admin = "admin",
+    super_Admin = "super_Admin",
+    manager = "manager",
+}
+console.log(UserRoles.admin);
+
+enum Status {
+    aboundend = "500",
+    notFound = "404",
+    Valid = "200"
+}
+console.log(Status.Valid);
